e420aa16c3bdbce897de99ad6610ba5b273d7370
sahesseling <35749568+sahesseling@users.noreply.github.com>
2018-04-22 08:55:07 -0500

    Transition Log v1 + Bug Fixes
    TransitionLog.java
    
    - Still has some issues with updating the customer table, but most entry and delete functions are working. 
    - Hope to have that issue fixed soon.
    
    - For some reason, doesn't like deleting gift-card only transactions. Will look into it, but not the highest priority. 
    
    GC.java
    
    - Fixed some interaction issues it had with the table and adapted some methods I learned with the Transition Log.
    
    customer.sql
    
    - Had to set most of the values to default null to allow the GC.java program to work properly. Was denying some entries due to blank values.

--- customer.sql
+++ customer.sql
@@ -1,14 +1,16 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `MovieTheaterMadness`
+-- Database: `mtm`
 --
 
 -- --------------------------------------------------------
@@ -17,29 +19,32 @@ SET time_zone = "+00:00";
 -- Table structure for table `customer`
 --
 
+DROP TABLE IF EXISTS `customer`;
 CREATE TABLE IF NOT EXISTS `customer` (
-  `fname` varchar(35) NOT NULL,
-  `lname` varchar(40) NOT NULL,
-  `age` int(3) DEFAULT NULL,
-  `movie_name` varchar(50) NOT NULL,
+  `fname` varchar(35) DEFAULT NULL,
+  `lname` varchar(40) DEFAULT NULL,
+  `age` int(3) NOT NULL DEFAULT '0',
+  `movie_name` varchar(50) DEFAULT NULL,
   `movie_time` time DEFAULT NULL,
-  `movie_price` varchar(10) NOT NULL,
-  `payment_method` varchar(20) NOT NULL,
-  `transaction_total` varchar(10) NOT NULL,
+  `movie_price` int(10) DEFAULT NULL,
+  `payment_method` varchar(20) DEFAULT NULL,
+  `transaction_total` int(10) NOT NULL,
   `order_id` int(11) NOT NULL AUTO_INCREMENT,
   `gc_bal` varchar(10) NOT NULL,
-  `gc_id` int(11) NOT NULL,
-  PRIMARY KEY (`order_id`),
-  UNIQUE KEY `gc_id` (`gc_id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+  `gc_id` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`order_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `customer`
 --
 
 INSERT INTO `customer` (`fname`, `lname`, `age`, `movie_name`, `movie_time`, `movie_price`, `payment_method`, `transaction_total`, `order_id`, `gc_bal`, `gc_id`) VALUES
-('John', 'Smith', 35, 'Ready Player One', '12:00:00', '10', 'Cash', '10.60', 1, '0', 0),
-('Jack', 'Carter', 44, 'Ready Player One', '12:10:00', '10', 'Cash', '25.60', 4, '15', 1023);
+('Jack', 'Carter', 35, 'Test', '00:00:12', 10, 'cash', 11, 1, '', 0),
+('Jack', 'Carter', 35, 'Test', '00:00:12', 10, 'cash', 11, 2, '', 0),
+('John', 'Smith', 50, 'Test', '01:50:00', 15, 'GC', 17, 3, '33', 2589),
+('John', 'Smith', 50, 'Test', '01:50:00', 15, 'GC', 17, 4, '33', 2589);
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
